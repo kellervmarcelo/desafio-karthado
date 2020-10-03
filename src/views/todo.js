0bf5a010298616b9d91ceebce5354d11f06acec2
Marcelo Keller <33509703+kellervmarcelo@users.noreply.github.com>
2020-10-03 19:22:44 -0300

    Updated todo.js because the create method was missing

--- src/views/todo.js
+++ src/views/todo.js
@@ -19,6 +19,24 @@ const PostTitle = ({ record }) => {
   return <span>Post {record ? `"${record.title}"` : ""}</span>;
 };
 
+export const TodoCreate = (props) => (
+  <Create {...props}>
+    <SimpleForm>
+      <ReferenceInput source="userId" reference="users">
+        <SelectInput optionText="name" />
+      </ReferenceInput>
+      <TextInput source="title" />
+      <RadioButtonGroupInput
+        source="category"
+        choices={[
+          { id: "false", name: "Completada" },
+          { id: "true", name: "Não completada" },
+        ]}
+      />
+    </SimpleForm>
+  </Create>
+);
+
 export const TodoEdit = (props) => (
   <Edit title={<PostTitle />} {...props}>
     <SimpleForm>
